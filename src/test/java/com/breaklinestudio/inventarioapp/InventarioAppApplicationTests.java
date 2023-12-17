04d96221c347b14728dfb88e8f26a28ca906206f
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2023-12-17 10:08:01 -0500

    Proyecto Inv Spring

---
+++ src/test/java/com/breaklinestudio/inventarioapp/InventarioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breaklinestudio.inventarioapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
